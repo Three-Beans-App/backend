9afdc42fd1ed5a19027d9b584743ca97c764bf5d
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-09 20:13:27 +1000

    feat: created user model, added bcrypt as a dependency

---
+++ src/models/UserModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    birthday: {
+        type: String,
+        required: false
+    }
+});
+
+userSchema.pre(
+    "save",
+    async function (next) {
+        const user = this;
+
+        if (!user.isModified("password")) {
+            return next();
+        }
+
+        try {
+            const hash = await bcrypt.hash(this.password, 10);
+            this.password = hash;
+            next();
+        } catch (err) {
+            next(err);
+        }
+    });
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = {
+    UserModel
+}
