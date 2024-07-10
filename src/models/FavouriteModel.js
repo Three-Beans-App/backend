52444037598696eecf5b09b854bb01efc2116f00
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-10 19:31:44 +1000

    feat: created FavouriteModel

---
+++ src/models/FavouriteModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { UserModel } = require('./UserModel.js');
+const { ItemModel } = require('./ItemModel.js');
+
+
+const favouriteItemSchema = new mongoose.Schema({
+    itemId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    customizations: {
+        size: {
+            type: String,
+            required: false
+        },
+        milk: {
+            type: String,
+            required: false
+        },
+        sugar: {
+            type: String,
+            required: false
+        }
+    }
+});
+
+
+const favouriteSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    item: favouriteItemSchema
+});
+
+const FavouriteModel = mongoose.model('Favourite', favouriteSchema);
+
+module.exports = {
+    FavouriteModel
+}
