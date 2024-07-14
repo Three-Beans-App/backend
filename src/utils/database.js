1c8d3877873784c44b0614dbef44b53e477ee080
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-14 20:29:41 +1000

    utils: added databaseClose and databaseClear admin functions

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
 
-async function databaseConnect(){
+async function databaseConnect() {
     let databaseURL = process.env.DATABASE_URL || "mongodb://localhost:27017/three-beans-db";
 
     await mongoose.connect(databaseURL);
     console.log("Database connecting completed!")
 }
 
+async function databaseClose() {
+    await mongoose.connection.close();
+    console.log("Database is disconnected");
+}
+
+async function databaseClear() {
+    await mongoose.connection.db.dropDatabase()
+}
+
 module.exports = {
-    databaseConnect
+    databaseConnect,
+    databaseClose,
+    databaseClear
 }
