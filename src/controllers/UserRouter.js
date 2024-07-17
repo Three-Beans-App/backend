618764d237e61341b9158db7fbbc0d2338598cea
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-17 22:04:45 +1000

    feat: created POST route for user signup

--- src/controllers/UserRouter.js
+++ src/controllers/UserRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const { UserModel } = require('../models/UserModel');
 const { comparePasswords, createJwt } = require('../utils/auth');
 const router = express.Router();
 
+// Base route to get all users
 router.get("/", async (request, response, next) => {
     let result = await UserModel.find({}).exec();
 
@@ -11,6 +13,46 @@ router.get("/", async (request, response, next) => {
         result: result
     });
 });
+
+// User signup route
+router.post("/signup", async (request, response, next) => {
+    const { email, password, name, birthday } = request.body;
+
+    try {
+        // Check email for existing user
+        const existingUser = await UserModel.findOne({ email }).exec();
+        if (existingUser) {
+            return response.status(400).json({
+                message: "It looks like you've already made a profile with this email."
+            });
+        }
+        // Hash the password with bcrypt
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user from request body
+        const newUser = new UserModel({
+            email,
+            password: hashedPassword,
+            name,
+            birthday
+        });
+        // Save the user to the database
+        await newUser.save();
+        // Create JWT for the new user
+        const token = createJwt(newUser._id);
+
+        // Respond with JWT and confirmation message 
+        response.status(201).json({
+            message: `Thank you for signing up to Three Beans ${name}!`,
+            token
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+
 
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const { app } = require("./server.js");
 const { databaseConnect } = require("./utils/database.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 app.listen(PORT, () => {
-    console.log("Server listening on http://localhost:3000");
+    console.log("Server listening on http://localhost:3001");
 
     // After server succesfully starts, connect to the database
     databaseConnect()
