8195d8247f9fcd9d54095034afc5c097429cdc63
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-24 00:08:58 +1000

    feat: routes for categories and items of a specific category

--- src/controllers/MenuRouter.js
+++ src/controllers/MenuRouter.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const { ItemModel } = require('../models/ItemModel');
+const { ItemModel, CategoryModel } = require('../models/ItemModel');
 const router = express.Router();
 
 
 // Base route to get all items
 router.get("/", async (request, response, next) => {
-    // query database to find all item documents
-    let items = await ItemModel.find({}).exec();
+    try{
+        // query database to find all item documents
+        const items = await ItemModel.find({}).exec();
 
-    response.json({
-        // respond with each item
-        result: items
-    });
+        response.json({
+            // respond with each item
+            result: items
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+// Route to get all categories
+router.get("/categories", async (request, response, next) => {
+    try{
+        const categories = await CategoryModel.find({}).exec();
+        response.json({
+            result: categories
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
 
@@ -33,6 +50,27 @@ router.get("/:id", async (request, response, next) => {
         response.json({
             // respond with the matching item
             result: item
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+// Route to get all items of a specific category
+router.get("/categories/:categoryId", async (request, response, next) => {
+    try{
+        const categoryId = request.params.categoryId;
+        const items = await ItemModel.find({ category: categoryId }).exec();
+
+        if (items.length === 0) {
+            return response.status(404).json({
+                message: "There are currently no items in this category."
+            });
+        }
+
+        response.json({
+            result: items
         });
     } catch (error) {
         next(error);
