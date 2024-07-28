d3ba76ff80e6becf7787ac0e9abca410940e0df7
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-28 19:49:51 +1000

    feat: GET request to view all orders

--- src/controllers/OrderRouter.js
+++ src/controllers/OrderRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { OrderModel } = require('../models/OrderModel');
-const { verifyJwt, validateObjectId } = require('../utils/middleware');
+const { verifyJwt, validateObjectId, verifyAdmin } = require('../utils/middleware');
 const { toNamespacedPath } = require('path');
 const router = express.Router();
 
@@ -16,6 +16,23 @@ router.get(
             const orders = await OrderModel.find({
                 user: id
             }).sort({
+                date: -1
+            }).exec();
+            response.status(200).json({ orders });
+        } catch (error) {
+            next(error);
+        }
+});
+
+
+// Route for admins to view all orders
+router.get(
+    "/",
+    verifyJwt,
+    verifyAdmin,
+    async (request, response, next) => {
+        try {
+            const orders = await OrderModel.find({}).sort({
                 date: -1
             }).exec();
             response.status(200).json({ orders });
