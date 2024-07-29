569940da61870b4253e5f7b56bffe3d71c096726
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-29 21:54:26 +1000

    test: tests for verifyJwt

--- tests/middleware.test.js
+++ tests/middleware.test.js
@@ -43,5 +43,58 @@ describe('Middleware', () => {
             });
             expect(next).not.toHaveBeenCalled();
         });
-    })
+    });
+
+
+    describe('verifyJwt', () => {
+        it('should call next if token is valid', () => {
+            const userId = new mongoose.Types.ObjectId().toString();
+            const token = jwt.sign({ id: userId }, process.env.JWT_KEY);
+            const request = { headers: { authorization: `Bearer ${token}` } };
+            const response = {};
+            const next = jest.fn();
+
+            verifyJwt(request, response, next);
+
+            expect(request.userId).toBe(userId);
+            expect(next).toHaveBeenCalled();
+        });
+
+        it('should return an error if no token is provided', () => {
+            const request = { headers: {} };
+            const response = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+            const next = jest.fn();
+
+            verifyJwt(request, response, next);
+
+            expect(response.status).toHaveBeenCalledWith(401);
+            expect(response.json).toHaveBeenCalledWith({
+                message: "Authentication token is required"
+            });
+            expect(next).not.toHaveBeenCalled();
+        });
+
+        it('should return an error if the token provided is invalid', () => {
+            const request = { headers: { authorization: 'Bearer invalid-token'} };
+            const response = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+            const next = jest.fn();
+
+            verifyJwt(request, response, next);
+
+            expect(response.status).toHaveBeenCalledWith(401);
+            expect(response.json).toHaveBeenCalledWith({
+                message: "Invalid token"
+            });
+            expect(next).not.toHaveBeenCalled();
+        });
+    });
+
+
+
 });
