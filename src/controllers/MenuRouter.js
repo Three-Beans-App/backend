5dea3de803247b1d5eb52afcd933aed124410646
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-08-01 21:51:31 +1000

    fix: added code comments

--- src/controllers/MenuRouter.js
+++ src/controllers/MenuRouter.js
@@ -14,8 +14,10 @@ router.post(
     verifyJwt, 
     verifyAdmin, 
     async (request, response, next) => {
+        // Retrive item details from request body
         const { name, category, price, description, image } = request.body;
         try {
+            // Check the category in the request exists 
             const categoryDocument = await CategoryModel.findOne({
                  name: category
             }).exec();
@@ -24,7 +26,7 @@ router.post(
                     message: "Category not found."
                 });
             }
-
+            // Check to ensure the item doesn't already exist
             const existingItem = await ItemModel.findOne({
                 name: name
             });
@@ -33,7 +35,7 @@ router.post(
                     message: "An item with this name already exists."
                 });
             }
-
+            // Create a new item document and save to the database
             const newItem = new ItemModel({
                 name,
                 category: categoryDocument._id,
@@ -42,6 +44,7 @@ router.post(
                 image
             });
             await newItem.save();
+            // Return confirmation and the new item as a response
             response.status(201).json({
                 message: "Item added successfully.",
                 item: newItem
@@ -59,18 +62,21 @@ router.post(
     verifyJwt,
     verifyAdmin,
     async (request, response, next) => {
+        // Take the category name from the request body
         const { name } = request.body;
         try {
+            // Check to ensure the category doesn't already exist
             const existingCategory = await CategoryModel.findOne({ name }).exec();
             if (existingCategory) {
                 return response.status(400).json({
                     message: "A category with this name already exists."
                 });
             }
-
+            // Create a new category document and save to the database
             const newCategory = new CategoryModel({ name });
             await newCategory.save();
 
+            // Return confirmation and the new category as a response
             response.status(201).json({
                 message: "Category added successfully",
                 category: newCategory
@@ -88,11 +94,8 @@ router.post(
 // eg. GET localhost:3001/menu/items/
 router.get("/items", async (request, response, next) => {
     try{
-        // query database to find all item documents
         const items = await ItemModel.find({}).exec();
-
         response.status(200).json({
-            // respond with each item
             result: items
         });
     } catch (error) {
@@ -119,20 +122,20 @@ router.get("/categories", async (request, response, next) => {
 // eg. GET localhost:3001/menu/item/669fb799d22cf89ef1ba3315/
 router.get("/item/:id", validateObjectId, async (request, response, next) => {
     try {
-        // include itemID in the search params
-        const { id } = request.params;       
-        // query database to find matching item for the ID
+        // Retrieve the item ID from request params
+        // Search the database for a matching item
+        const { id } = request.params;
         const item = await ItemModel.findById(id).exec();
 
         if (!item) {
-            // return a 404 if no item with the ID exists
+            // Return a 404 if no item with the ID exists
             return response.status(404).json({
                 message: "Item not found."
             });
         }
 
         response.status(200).json({
-            // respond with the matching item
+            // Respond with the matching item
             result: item
         });
     } catch (error) {
@@ -145,7 +148,8 @@ router.get("/item/:id", validateObjectId, async (request, response, next) => {
 // eg. GET localhost:3001/menu/category/669fb799d22cf89ef1ba330c/
 router.get("/category/:id", validateObjectId, async (request, response, next) => {
     try{
-        // Retrieve category from request params and search for items
+        // Retrieve category ID from request params
+        // Search the database for a matching category
         const { id } = request.params;
         const items = await ItemModel.find({ category: id }).exec();
 
@@ -176,13 +180,16 @@ router.patch(
     verifyJwt, 
     verifyAdmin, 
     async (request, response, next) => {
+        // Retrieve item ID from request params
+        // Retrive update details from the request body
         const { id } = request.params;
         const { name, category, price, description, image } = request.body;
         try {
             const updateFields = {};
-
+            // Conditional logic to update the values in the updateFields object
             if (name) updateFields.name = name;
             if (category) {
+                // Check to ensure the category exists if included
                 const categoryDocument = await CategoryModel.findOne({
                     name: category
                 }).exec();
@@ -197,6 +204,7 @@ router.patch(
             if (description) updateFields.description = description;
             if (image) updateFields.image = image;
 
+            // Find and update the item document in the database
             const item = await ItemModel.findByIdAndUpdate(
                 id,
                 updateFields,
@@ -206,11 +214,16 @@ router.patch(
                 }
             ).exec();
             if (!item) {
+                // Return 404 if no item is found from search params
                 return response.status(404).json({
                     message: "Item not found" 
                 });
             }
-            response.status(200).json({ message: "Item updated successfully", item });
+            // Return confirmation and the updated item document as the response
+            response.status(200).json({ 
+                message: "Item updated successfully", 
+                item 
+            });
         } catch (error) {
             next(error);
         }
@@ -225,15 +238,19 @@ router.patch(
     verifyJwt,
     verifyAdmin,
     async (request, response, next) => {
+        // Retrieve category ID from Request params
+        // Retrieve updated category name from request body
         const { id } = request.params;
         const { name } = request.body;
         try {
+            // Check to ensure the updated name doesn't already exist
             const existingCategory = await CategoryModel.findOne({ name }).exec();
             if (existingCategory) {
                 return response.status(400).json({
                     message: "Category with this name already exists."
                 });
             }
+            // Find and uodate the category document in the database
             const category = await CategoryModel.findByIdAndUpdate(
                 id, 
                 { name },
@@ -243,11 +260,13 @@ router.patch(
                 }
             ).exec();
 
+            // Return 404 if no category found from the request params
             if (!category) {
                 return response.status(404).json({
                     message: "Category not found."
                 });
             }
+            // Return confirmation and the uodated category as the response
             response.status(200).json({
                 message: "Category updated successfully",
                 category
@@ -269,14 +288,19 @@ router.delete(
     verifyJwt, 
     verifyAdmin, 
     async (request, response, next) => {
+        // Retrieve item ID from request params
         const { id } = request.params;       
         try {
+            // Find and delete the item document in the database
             const item = await ItemModel.findByIdAndDelete(id).exec();
+
+            // Return a 404 if no item is found from request params
             if (!item) {
                 return response.status(404).json({
                     message: "Item not found."
                 });
             }
+            // Return confirmation as the response
             response.status(200).json({
                 message: `Item ${item.name} deleted successfully.`
             });
@@ -294,20 +318,26 @@ router.delete(
     verifyJwt,
     verifyAdmin,
     async (request, response, next) => {
+        // Retrieve category ID from the request params
         const { id } = request.params;
         try {
+            // Check to ensure no items are associated with the category
             const itemsInCategory = await ItemModel.find({ category: id }).exec();
             if (itemsInCategory.length > 0) {
                 return response.status(400).json({
                     message: "Cannot delete this category as there are still items associated with it."
                 });
             }
+            // FInd the category document and delete it in the database
             const category = await CategoryModel.findByIdAndDelete(id).exec();
+
+            // Return a 404 if no category is found from the request params
             if (!category) {
                 return response.status(404).json({
                     message: "Category not found."
                 });
             }
+            // Return confirmation as the response
             response.status(200).json({
                 message: `Category ${category.name} deleted successfully.`
             });
