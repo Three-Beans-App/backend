f0072e020dc2562a434550858e3be07e206974c6
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-08-05 20:16:18 +1000

    fix: adjusted log on app.listen

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 
 app.listen(PORT, () => {
-    console.log("Server listening on http://localhost:3001");
+    console.log(`Server listening on port ${PORT}`);
 
     // After server succesfully starts, connect to the database
     databaseConnect()
